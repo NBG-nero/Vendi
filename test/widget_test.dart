dc6da11243f5b617d033b7a99d307f4d9c537873
Aphra <oreofeojomo@gmail.com>
2023-01-08 13:34:48 +0100

    feat: setup in progress

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:vendi/app_widget.dart';
 
-import 'package:vendi/main.dart';
+// import 'package:vendi/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( Vendi());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
