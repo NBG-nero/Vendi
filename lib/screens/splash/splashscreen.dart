d26b28ebc20167ce530a8c883f01b1cdd2c53448
Aphra <oreofeojomo@gmail.com>
2023-01-09 14:05:43 +0100

    feat: splash

--- lib/screens/splash/splashscreen.dart
+++ lib/screens/splash/splashscreen.dart
@@ -1,12 +1,20 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Splashscreen extends StatelessWidget {
   const Splashscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor:Colors.blue.shade50,
+        body: Center(
+            child: Text(
+      "VENDI",
+      style: TextStyle(
+          fontSize: 100.sp,
+          fontStyle: FontStyle.italic,
+          color: Colors.amber[600]),
+    )));
   }
 }
