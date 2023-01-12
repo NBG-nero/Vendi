7a2bc7334b664a11265a1ea65dfc3763e6921605
Aphra <oreofeojomo@gmail.com>
2023-01-12 19:33:00 +0100

    feat: locator

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app_widget.dart';
+import 'utilities/locator.dart';
 
 void main() async {
   await ScreenUtil.ensureScreenSize();
+  setUpLocator();
   runApp(Vendi());
 }
