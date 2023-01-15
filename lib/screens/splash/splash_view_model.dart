0f73e15b96c308abc6f726484214455de5a3007d
Aphra <oreofeojomo@gmail.com>
2023-01-15 17:17:50 +0100

    feat: minor changes

--- lib/screens/splash/splash_view_model.dart
+++ lib/screens/splash/splash_view_model.dart
@@ -1,4 +1,4 @@
- import'BaseModel.dart';
+ import '../base_model.dart';
  
  class SplashViewModel extends BaseModel{ 
   
--- lib/screens/splash/splashscreen.dart
+++ lib/screens/splash/splashscreen.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stacked/stacked.dart';
+import 'package:vendi/screens/splash/splash_view_model.dart';
 
 class Splashscreen extends StatelessWidget {
   const Splashscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-        backgroundColor: Colors.blue.shade50,
-        body: Center(
-            child: Text(
-          "VENDI",
-          style: TextStyle(
-              fontSize: 100.sp,
-              fontStyle: FontStyle.italic,
-              color: Colors.amber[600]),
-        )));
+    return ViewModelBuilder<SplashViewModel>.reactive(
+        viewModelBuilder: () => SplashViewModel(),
+        onModelReady: (s) {
+          s.setInitialised(true);
+          Future.delayed(const Duration(seconds: 5), (() { 
+            
+          }));
+        },
+        builder: (context, model, child) {
+          return Scaffold(
+              backgroundColor: Colors.blue.shade50,
+              body: Center(
+                  child: Text(
+                "VENDI",
+                style: TextStyle(
+                    fontSize: 100.sp,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.amber[600]),
+              )));
+        });
   }
 }
