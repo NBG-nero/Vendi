ebc03615fd1e4c3b4a4e7ae2031b5cf7170ad61c
Aphra <oreofeojomo@gmail.com>
2023-01-18 16:58:28 +0100

    feat:colors

--- lib/screens/splash/splashscreen.dart
+++ lib/screens/splash/splashscreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stacked/stacked.dart';
 import 'package:vendi/screens/splash/splash_view_model.dart';
+import 'package:vendi/utilities/constants/constants.dart';
 
 import '../../routes/router.gr.dart';
 
@@ -29,14 +30,14 @@ class Splashscreen extends StatelessWidget {
         },
         builder: (context, model, child) {
           return Scaffold(
-              backgroundColor: Colors.blue.shade50,
+              backgroundColor: VendiColors.primaryColor,
               body: Center(
                   child: Text(
                 "VENDI",
                 style: TextStyle(
                     fontSize: 100.sp,
                     fontStyle: FontStyle.italic,
-                    color: Colors.amber[600]),
+                    color: VendiColors.tertiaryColor),
               )));
         });
   }
---
+++ lib/utilities/constants/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class VendiColors {
+  VendiColors._();
+
+  static Color primaryColor = const Color.fromARGB(255, 227, 242, 253);
+  static Color secondaryColor = const Color.fromARGB(255, 144, 202, 249);
+  static Color tertiaryColor = const  Color.fromARGB(255, 255, 179, 0);
+  static Map<int, Color> colorMap = {
+    50: const Color.fromARGB(255, 227, 242, 253),
+    100: const Color.fromARGB(255, 187, 222, 251),
+    200: const Color.fromARGB(255, 144, 202, 249),
+    300: const Color.fromARGB(255, 100, 181, 246),
+    400: const Color.fromARGB(255, 66, 165, 245),
+    500: const Color.fromARGB(255, 33, 150, 243),
+    600: const Color.fromARGB(255, 30, 136, 229),
+    700: const Color.fromARGB(255, 25, 118, 210),
+    800: const Color.fromARGB(255, 21, 101, 192),
+    900: const Color.fromARGB(255, 13, 71, 161),
+  };
+  static MaterialColor primaryColorswatch = MaterialColor(0xFF0D47A1, colorMap);
+}
