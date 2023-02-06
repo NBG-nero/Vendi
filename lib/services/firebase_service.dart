31c50cc0f76f9b0b1ecb61cff9d14184d0cfd776
Aphra <oreofeojomo@gmail.com>
2023-02-06 13:48:13 +0100

    feat: adduser

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,8 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:developer';
 
+import 'authservice.dart';
 
 class FirebaseService {
-  CollectionReference homeBanner = FirebaseFirestore.instance.collection('homeBanner');
-  
+  AuthService authService = AuthService();
+  CollectionReference homeBanner =
+      FirebaseFirestore.instance.collection('homeBanner');
 
+  addUser(name, email) async {
+    final dataBase = FirebaseFirestore.instance;
+    final personalEmail = await authService.getCurrentEmail();
+    log("Adduser was called");
+    await dataBase
+        .collection('userData')
+        .doc(personalEmail)
+        .set({"name": name, "email": email}).catchError((e) {
+      log("AddUser failed");
+    });
+  }
 }
