dc36449c3a82d23268cd939c29f03ab7cac97868
Aphra <oreofeojomo@gmail.com>
2023-02-07 13:59:04 +0100

    feat:createuser

--- lib/services/authservice.dart
+++ lib/services/authservice.dart
@@ -2,8 +2,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth auth = FirebaseAuth.instance;
-  Stream<User?> get authStateChanges => auth.idTokenChanges();
+
+  Stream<User?> authStateChanges() {
+    return auth.idTokenChanges();
+  }
+
   Future getCurrentEmail() async {
     return auth.currentUser?.email;
+  }
+
+  Future<String?> createUserWithEmailAndPassword(
+      String? name, String email, String password) async {
+    final User? currentUser = (await auth.createUserWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    return currentUser?.uid;
   }
 }
